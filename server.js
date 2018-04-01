c260d103cd61a08c98792da1bc32790d80e6f4ac
Shahin Sheidaei <sheidaei@Shahins-MacBook-Pro.local>
2018-03-31 22:22:58 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+var port = process.env.PORT || 3000;
+
+app.get('/', (request, response) => {
+  response.send('Hello from Express!')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
